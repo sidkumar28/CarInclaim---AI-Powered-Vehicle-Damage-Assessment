5108581e837ad43955887375192214b93428ae89
SIDKUMAR <sidofficial28@gmail.com>
2026-02-13 20:55:51 +0530

    feat: production nginx reverse proxy setup

--- insurance-damage-frontend/src/App.jsx
+++ insurance-damage-frontend/src/App.jsx
@@ -7,7 +7,8 @@ const generateRequestId = () => {
   return uuidv4();
 };
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "/api";
+
 
 function App() {
   const [file, setFile] = useState(null);
